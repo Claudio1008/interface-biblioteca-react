7dd89b611358c81aac3826f190e52159feb4c125
Claudio Gabriel G Batista <claudiogabrielbatista7@gmail.com>
2025-05-07 16:48:29 -0300

    fazendo o cadastro do livro

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,4 @@
+import { JSX } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { APP_ROUTES } from './appConfig';
 import PHome from './pages/PHome/PHome';
@@ -5,12 +6,14 @@ import PLogin from './pages/PLogin/PLogin';
 import PListaAlunos from './pages/PListagem/PListaAlunos/PListaAlunos';
 import PListaEmprestimos from './pages/PListagem/PListaEmprestimos/PListaEmprestimos';
 import PListagemLivros from './pages/PListagem/PListaLivros/PListaLivros';
+import PCadastroAluno from './pages/PCadastro/PCadastroAluno/PCadastroAluno';
+import PCadastroLivro from './pages/PCadastro/PCadastroLivro/PCadastroLivro';
 
 /**
  * Componente que irá lidar com todas as rotas da aplicação
  * @returns Um componente web para lidar com as rotas
  */
-function AppRoutes() {
+function AppRoutes(): JSX.Element {
     return (
         <BrowserRouter>
             <Routes>
@@ -25,6 +28,9 @@ function AppRoutes() {
                 <Route path={APP_ROUTES.ROUTE_LISTAGEM_EMPRESTIMOS} element={<PListaEmprestimos />} />
                 {/* Quando a rota representada pela variável ROUTE_LISTAGEM_LIVROS é acessada, renderiza a página PListagemLivros */}
                 <Route path={APP_ROUTES.ROUTE_LISTAGEM_LIVROS} element={<PListagemLivros />} />
+            
+                <Route path={APP_ROUTES.ROUTE_CADASTRO_ALUNO} element={<PCadastroAluno />} />
+                <Route path={APP_ROUTES.ROUTE_CADASTRO_LIVRO} element={<PCadastroLivro />} />
             </Routes>
         </BrowserRouter>
     );
